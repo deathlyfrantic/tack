484536e728dc5f0fde7fb0601f0449e846f0e0ee
Zandr Martin <zandrmartin@gmail.com>
2018-07-18 07:44:58 -0400

    rename test macro assert() to test_assert()
    to avoid confusion with assert.h's assert() macro

--- test/test-score.c
+++ test/test-score.c
@@ -5,41 +5,41 @@
 
 void test_score_new() {
   Score *score = score_new();
-  assert(score->points == UINT_MAX);
-  assert(score->first == 0);
-  assert(score->last == 0);
-  assert(score->line == NULL);
+  test_assert(score->points == UINT_MAX);
+  test_assert(score->first == 0);
+  test_assert(score->last == 0);
+  test_assert(score->line == NULL);
 }
 
 void test_calculate_score() {
   // verify NULL is returned if line doesn't contain query chars
-  assert(calculate_score("foo", "qxz") == NULL);
+  test_assert(calculate_score("foo", "qxz") == NULL);
 
   Score *a = calculate_score("oof", "f");
-  assert(a->points == 1);
-  assert(a->first == 2);
-  assert(a->last == 2);
-  assert(strcmp(a->line, "oof") == 0); // just to verify line gets set
+  test_assert(a->points == 1);
+  test_assert(a->first == 2);
+  test_assert(a->last == 2);
+  test_assert(strcmp(a->line, "oof") == 0); // just to verify line gets set
 
   // this should be case insensitive
   Score *b = calculate_score("oof", "F");
-  assert(b != NULL);
-  assert(b->points == 1);
-  assert(b->first == 2);
-  assert(b->last == 2);
+  test_assert(b != NULL);
+  test_assert(b->points == 1);
+  test_assert(b->first == 2);
+  test_assert(b->last == 2);
 
   Score *c = calculate_score("foofbbar", "fob");
-  assert(c->points == 5);
-  assert(c->first == 0);
-  assert(c->last == 4);
+  test_assert(c->points == 5);
+  test_assert(c->first == 0);
+  test_assert(c->last == 4);
 
   Score *d = calculate_score("foo / ba r", "or");
-  assert(d->points == 2);
-  assert(d->first == 1);
-  assert(d->last == 9);
+  test_assert(d->points == 2);
+  test_assert(d->first == 1);
+  test_assert(d->last == 9);
 
   Score *e = calculate_score("f||||||||b||||||||||||||a||||f||||||||r", "bar");
-  assert(e->points == 2);
-  assert(e->first == 9);
-  assert(e->last == 38);
+  test_assert(e->points == 2);
+  test_assert(e->first == 9);
+  test_assert(e->last == 38);
 }
