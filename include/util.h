c595b0934d5cc24b57ff923c7fb94e7e70a012ec
Zandr Martin <zandrmartin@gmail.com>
2018-07-26 13:41:39 -0400

    combine expand_tabs and highlight_line into one fn

--- include/util.h
+++ include/util.h
@@ -8,5 +8,6 @@
 char *strtolower(const char *);
 unsigned int find_char_idx(const char *, char);
 unsigned short get_num_strlen(size_t);
+size_t count_chars_in_string(const char *, char);
 
 #endif /* end of include guard: TACK_UTIL_H */
--- test/test-render.c
+++ test/test-render.c
@@ -7,36 +7,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-void test_highlight_line() {
-  char expected1[] =
-      COLOR_RESET "foo" COLOR_RED "bar" COLOR_DEFAULT "baz" CLEAR_LINE "\r\n";
-  char *result1 = highlight_line("foobarbaz", 3, 6, false, true);
+void test_render_line() {
+  char expected1[] = COLOR_RESET "foo     " COLOR_RED "bar" COLOR_DEFAULT
+                                 "baz" CLEAR_LINE "\r\n";
+  char *result1 = render_line("foo\tbarbaz", 4, 7, false, true);
   test_assert(strcmp(expected1, result1) == 0);
   free(result1);
   char expected2[] = COLOR_RESET COLOR_REVERSE
       "foo" COLOR_RED "bar" COLOR_DEFAULT "baz" COLOR_RESET CLEAR_LINE "\r\n";
-  char *result2 = highlight_line("foobarbaz", 3, 6, true, true);
-  test_assert(strcmp(expected2, result2) == 0);
-  char expected3[] = COLOR_RESET "foobarbaz" CLEAR_LINE "\r\n";
-  free(result2);
-  char *result3 = highlight_line("foobarbaz", 0, 0, false, false);
-  test_assert(strcmp(expected3, result3) == 0);
-  free(result3);
-}
-
-void test_expand_tabs() {
-  char expected1[] = "foo     bar";
-  char *result1 = expand_tabs("foo\tbar");
-  test_assert(strcmp(expected1, result1) == 0);
-  free(result1);
-  char expected2[] = "bazquux garply";
-  char *result2 = expand_tabs("bazquux\tgarply");
+  char *result2 = render_line("foobarbaz", 3, 6, true, true);
   test_assert(strcmp(expected2, result2) == 0);
   free(result2);
-  char expected3[] = "foo     bar     baz     quux            garply";
-  char *result3 = expand_tabs("foo\tbar\tbaz\tquux\t\tgarply");
+  char expected3[] =
+      COLOR_RESET "foo     bar     baz     " COLOR_RED
+                  "quux            gar" COLOR_DEFAULT "ply" CLEAR_LINE "\r\n";
+  Score *score = calculate_score("foo\tbar\tbaz\tquux\t\tgarply", "qr");
+  char *result3 =
+      render_line(score->line, score->first, score->last, false, true);
   test_assert(strcmp(expected3, result3) == 0);
+  free(score);
   free(result3);
+  char expected4[] =
+      COLOR_RESET "foo" COLOR_RED "bar" COLOR_DEFAULT "baz" CLEAR_LINE "\r\n";
+  char *result4 = render_line("foobarbaz", 3, 6, false, true);
+  test_assert(strcmp(expected4, result4) == 0);
+  free(result4);
+  char expected5[] = COLOR_RESET COLOR_REVERSE
+      "foo" COLOR_RED "bar" COLOR_DEFAULT "baz" COLOR_RESET CLEAR_LINE "\r\n";
+  char *result5 = render_line("foobarbaz", 3, 6, true, true);
+  test_assert(strcmp(expected5, result5) == 0);
+  free(result5);
+  char expected6[] = COLOR_RESET "foobarbaz" CLEAR_LINE "\r\n";
+  char *result6 = render_line("foobarbaz", 0, 0, false, false);
+  test_assert(strcmp(expected6, result6) == 0);
+  free(result6);
+  char expected7[] = COLOR_RESET "foo     bar" CLEAR_LINE "\r\n";
+  char *result7 = render_line("foo\tbar", 0, 0, false, false);
+  test_assert(strcmp(expected7, result7) == 0);
+  free(result7);
+  char expected8[] = COLOR_RESET "bazquux garply" CLEAR_LINE "\r\n";
+  char *result8 = render_line("bazquux\tgarply", 0, 0, false, false);
+  test_assert(strcmp(expected8, result8) == 0);
+  free(result8);
 }
 
 void test_renderer_render() {
@@ -54,9 +66,9 @@ void test_renderer_render() {
   r->height = 15;
   r->selected = 1;
   r->match_length = get_num_strlen(scores->length);
-  char *output = renderer_render(r);
-  puts(output);
-  free(output);
+  // char *output = renderer_render(r);
+  // puts(output);
+  // free(output);
   free(a);
   free(b);
   free(c);
