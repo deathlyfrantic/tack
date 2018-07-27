795037e61067bab165ac338a052482685144b508
Zandr Martin <git@zandr.me>
2018-07-26 21:05:12 -0400

    remove blank lines

--- test/test-score.c
+++ test/test-score.c
@@ -15,14 +15,12 @@ void test_score_new() {
 void test_calculate_score() {
   // verify NULL is returned if line doesn't contain query chars
   test_assert(calculate_score("foo", "qxz") == NULL);
-
   Score *a = calculate_score("oof", "f");
   test_assert(a->points == 1);
   test_assert(a->first == 2);
   test_assert(a->last == 3);
   test_assert(strcmp(a->line, "oof") == 0); // just to verify line gets set
   free(a);
-
   // this should be case insensitive
   Score *b = calculate_score("oof", "F");
   test_assert(b != NULL);
@@ -30,19 +28,16 @@ void test_calculate_score() {
   test_assert(b->first == 2);
   test_assert(b->last == 3);
   free(b);
-
   Score *c = calculate_score("foofbbar", "fob");
   test_assert(c->points == 5);
   test_assert(c->first == 0);
   test_assert(c->last == 5);
   free(c);
-
   Score *d = calculate_score("foo / ba r", "or");
   test_assert(d->points == 2);
   test_assert(d->first == 1);
   test_assert(d->last == 10);
   free(d);
-
   Score *e = calculate_score("f||||||||b||||||||||||||a||||f||||||||r", "bar");
   test_assert(e->points == 2);
   test_assert(e->first == 9);
