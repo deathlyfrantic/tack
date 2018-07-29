e77df9bd0c03a11e5fc9d47dc1d17df8f2d3b2f9
Zandr Martin <git@zandr.me>
2018-07-29 09:24:37 -0400

    adjust naming of scores in test-score.c

--- test/test-score.c
+++ test/test-score.c
@@ -15,32 +15,33 @@ void test_score_new() {
 void test_calculate_score() {
   // verify NULL is returned if line doesn't contain query chars
   test_assert(calculate_score("foo", "qxz") == NULL);
-  Score *a = calculate_score("oof", "f");
-  test_assert(a->points == 1);
-  test_assert(a->first == 2);
-  test_assert(a->last == 3);
-  test_assert(strcmp(a->line, "oof") == 0); // just to verify line gets set
-  free(a);
+  Score *score1 = calculate_score("oof", "f");
+  test_assert(score1->points == 1);
+  test_assert(score1->first == 2);
+  test_assert(score1->last == 3);
+  test_assert(strcmp(score1->line, "oof") == 0); // just to verify line gets set
+  free(score1);
   // this should be case insensitive
-  Score *b = calculate_score("oof", "F");
-  test_assert(b != NULL);
-  test_assert(b->points == 1);
-  test_assert(b->first == 2);
-  test_assert(b->last == 3);
-  free(b);
-  Score *c = calculate_score("foofbbar", "fob");
-  test_assert(c->points == 5);
-  test_assert(c->first == 0);
-  test_assert(c->last == 5);
-  free(c);
-  Score *d = calculate_score("foo / ba r", "or");
-  test_assert(d->points == 2);
-  test_assert(d->first == 1);
-  test_assert(d->last == 10);
-  free(d);
-  Score *e = calculate_score("f||||||||b||||||||||||||a||||f||||||||r", "bar");
-  test_assert(e->points == 2);
-  test_assert(e->first == 9);
-  test_assert(e->last == 39);
-  free(e);
+  Score *score2 = calculate_score("oof", "F");
+  test_assert(score2 != NULL);
+  test_assert(score2->points == 1);
+  test_assert(score2->first == 2);
+  test_assert(score2->last == 3);
+  free(score2);
+  Score *score3 = calculate_score("foofbbar", "fob");
+  test_assert(score3->points == 5);
+  test_assert(score3->first == 0);
+  test_assert(score3->last == 5);
+  free(score3);
+  Score *score4 = calculate_score("foo / ba r", "or");
+  test_assert(score4->points == 2);
+  test_assert(score4->first == 1);
+  test_assert(score4->last == 10);
+  free(score4);
+  Score *score5 =
+      calculate_score("f||||||||b||||||||||||||a||||f||||||||r", "bar");
+  test_assert(score5->points == 2);
+  test_assert(score5->first == 9);
+  test_assert(score5->last == 39);
+  free(score5);
 }
