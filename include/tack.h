2716ea8f6989be2828972119e2acdec5d95f1bea
Zandr Martin <zandrmartin@gmail.com>
2018-07-29 09:20:03 -0400

    add commandline options

--- include/tack.h
+++ include/tack.h
@@ -1,4 +1,14 @@
 #ifndef TACK_TACK_H
 #define TACK_TACK_H
 
+#include <stdbool.h>
+
+#define VERSION "0.0.1"
+
+typedef struct {
+  bool full_height;
+  unsigned short height;
+  char *initial_search;
+} Config;
+
 #endif /* end of include guard: TACK_TACK_H */
