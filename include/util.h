a4db74a3c65eb51ef796936cb24cd87aef4726b9
Zandr Martin <git@zandr.me>
2018-08-02 08:41:30 -0400

    remove unsigned qualifier from find_char_idx()
    uh how could it be unsigned if it returns -1 when char is not found?

--- include/util.h
+++ include/util.h
@@ -6,7 +6,7 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 char *strtolower(const char *);
-unsigned int find_char_idx(const char *, char);
+int find_char_idx(const char *, char);
 unsigned short get_num_strlen(size_t);
 size_t count_chars_in_string(const char *, char);
 
