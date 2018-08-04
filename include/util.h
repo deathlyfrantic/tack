735b7027f7dfa3ec115536ae025a04630d62a099
Zandr Martin <git@zandr.me>
2018-08-03 21:46:11 -0400

    remove count_chars_in_string() function

--- include/util.h
+++ include/util.h
@@ -9,6 +9,5 @@
 char *strtolower(const char *);
 int16_t find_char_idx(const char *, char);
 uint8_t get_num_strlen(size_t);
-size_t count_chars_in_string(const char *, char);
 
 #endif /* end of include guard: TACK_UTIL_H */
