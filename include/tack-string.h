deathlyfrantic/tack

commit f78657a3fd0cc73d2e8b0a1623b9bb5f332f0dfc

strings in find_end_of_match and regular_query

src/score.c
@@ -36,22 +36,22 @@ static bool single_char_query(Score *score, String *query) {
   return true;
 }
 
-static bool find_end_of_match(struct match *m, const char *line,
-                              const char *query, size_t start) {
+static bool find_end_of_match(struct match *m, String *line, String *query,
+                              size_t start) {
   uint16_t last_index = start, score = 1;
-  int16_t index;
+  int32_t index;
   enum match_type last_match_type = MATCH_TYPE_NORMAL;
-  for (size_t i = 0; i < strlen(query); i++) {
-    if ((index = find_char_idx(line + last_index, query[i])) == -1) {
-      return false;
-    }
+  for (size_t i = 1; i < query->length; i++) {
+    index = string_find_ichar_from(line, string_get_char_at(query, i),
+                                   last_index + 1);
+    if (index == -1) return false;
     index += last_index;
     if (index == last_index + 1) {
       if (last_match_type != MATCH_TYPE_SEQUENTIAL) {
         last_match_type = MATCH_TYPE_SEQUENTIAL;
         score++;
       }
-    } else if (index > 0 && !isalnum(line[index - 1])) {
+    } else if (index > 0 && !isalnum(string_get_char_at(line, index))) {
       if (last_match_type != MATCH_TYPE_BOUNDARY) {
         last_match_type = MATCH_TYPE_BOUNDARY;
         score++;
@@ -67,12 +67,12 @@ static bool find_end_of_match(struct match *m, const char *line,
   return true;
 }
 
-static bool regular_query(Score *score, const char *query) {
+static bool regular_query(Score *score, String *query) {
   bool found_score = false;
   struct match m;
   for (size_t i = 0; i < score->line->length; i++) {
-    if (score->line->low[i] == query[0]) {
-      if (find_end_of_match(&m, score->line->low + 1, query + 1, i)) {
+    if (string_get_char_at(score->line, i) == string_get_char_at(query, 0)) {
+      if (find_end_of_match(&m, score->line, query, i)) {
         found_score = true;
         if (m.score < score->points) {
           score->points = m.score;
@@ -97,7 +97,7 @@ Score *calculate_score(String *line, String *query) {
     }
     break;
   default:
-    if (regular_query(score, query->low)) {
+    if (regular_query(score, query)) {
       return score;
     }
   }

commit 0bc5334727d096d28dc19d0bfea13625841afc0d

fix scoring bugs introduced in f78657a

include/tack-string.h
@@ -17,6 +17,7 @@ String *string_new();
 String *string_new_from(char *);
 String *string_dup(String *);
 char string_get_char_at(String *, size_t);
+char string_get_ichar_at(String *, size_t);
 void string_push_char(String *, char);
 char string_pop_char(String *);
 void string_concat(String *, const char *);

src/score.c
@@ -42,8 +42,8 @@ static bool find_end_of_match(struct match *m, String *line, String *query,
   int32_t index;
   enum match_type last_match_type = MATCH_TYPE_NORMAL;
   for (size_t i = 1; i < query->length; i++) {
-    index = string_find_ichar_from(line, string_get_char_at(query, i),
-                                   last_index + 1);
+    index =
+      string_find_ichar_from(line, string_get_char_at(query, i), last_index);
     if (index == -1) return false;
     index += last_index;
     if (index == last_index + 1) {
@@ -51,7 +51,7 @@ static bool find_end_of_match(struct match *m, String *line, String *query,
         last_match_type = MATCH_TYPE_SEQUENTIAL;
         score++;
       }
-    } else if (index > 0 && !isalnum(string_get_char_at(line, index))) {
+    } else if (index > 0 && !isalnum(string_get_char_at(line, index - 1))) {
       if (last_match_type != MATCH_TYPE_BOUNDARY) {
         last_match_type = MATCH_TYPE_BOUNDARY;
         score++;
@@ -71,12 +71,12 @@ static bool regular_query(Score *score, String *query) {
   bool found_score = false;
   struct match m;
   for (size_t i = 0; i < score->line->length; i++) {
-    if (string_get_char_at(score->line, i) == string_get_char_at(query, 0)) {
+    if (string_get_ichar_at(score->line, i) == string_get_ichar_at(query, 0)) {
       if (find_end_of_match(&m, score->line, query, i)) {
         found_score = true;
         if (m.score < score->points) {
           score->points = m.score;
-          score->last = m.last + 1;
+          score->last = m.last;
           score->first = i;
         }
       }
@@ -176,6 +176,14 @@ void test_calculate_score() {
   test_assert(score6->first = 3);
   test_assert(score6->last = 6);
   free(score6);
+  // apparently there are no tests that would catch case sensitivity? dumb
+  string_set(s, "FOOBAR");
+  string_set(query, "foo");
+  Score *score7 = calculate_score(s, query);
+  test_assert(score7->first == 0);
+  test_assert(score7->last == 3);
+  test_assert(score7->points == 3);
+  free(score7);
   string_free(s);
   string_free(query);
 }

src/string.c
@@ -40,6 +40,11 @@ char string_get_char_at(String *s, size_t i) {
   return s->buf[i];
 }
 
+char string_get_ichar_at(String *s, size_t i) {
+  if (i > s->length) return '\0';
+  return s->low[i];
+}
+
 void string_push_char(String *s, char c) {
   if (s->length == s->capacity) {
     string_grow(s);