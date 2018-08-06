72f0612605e6a68e6fe05f809d606770d26650e8
Zandr Martin <git@zandr.me>
2018-08-06 18:37:00 -0400

    remove a bunch of unused string functions

--- include/tack-string.h
+++ include/tack-string.h
@@ -15,7 +15,6 @@ typedef struct {
 
 String *string_new();
 String *string_new_from(char *);
-String *string_dup(String *);
 const char *string_raw(String *);
 const char *string_raw_low(String *);
 char string_get_char_at(String *, size_t);
@@ -27,15 +26,11 @@ void string_delete_word(String *);
 void string_set(String *, const char *);
 void string_reset(String *);
 bool string_equals(String *, const char *);
-bool string_iequals(String *, const char *);
-bool string_equals_string(String *, String *);
-bool string_iequals_string(String *, String *);
 void string_free(String *);
 int32_t string_find_char(String *, char);
 int32_t string_find_ichar(String *, char);
 int32_t string_find_char_from(String *, char, size_t);
 int32_t string_find_ichar_from(String *, char, size_t);
 size_t string_count_chars(String *, char);
-size_t string_count_ichars(String *, char);
 
 #endif /* end of include guard: TACK_STRING_H */
